1dd589e282d99522b6c39d2741987b12201037a6
Márcio Vinícius <marcio.campos@outlook.com>
2021-04-07 18:16:01 -0300

    feature: add read json file

--- pages/about.js
+++ pages/about.js
@@ -1,19 +1,27 @@
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-const About = (props) => {
+const About = ({ post }) => {
   const { t } = useTranslation('common');
 
-  return <h1>{t('about')}</h1>;
-  // return <h1>{props.teste}</h1>;
+  return (
+    <div>
+      <h1>{t('about')}</h1>
+      <p>{post.about}</p>
+    </div>
+  );
 };
 
 export const getStaticProps = async ({ locale }) => {
   console.log(locale);
+
+  let rawdata = require(`../public/locales/${locale.toLowerCase()}/common.json`);
+  let post = JSON.parse(JSON.stringify(rawdata));
+
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common'])),
-      teste: '<p>texto 1</p>',
+      post,
     },
   };
 };
