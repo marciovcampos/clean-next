7b55e95731387ff1d2946d76bb614b1ed4964b4f
Márcio Vinícius <marcio.campos@outlook.com>
2021-04-19 17:33:03 -0300

    add getAllTopics Function

--- pages/[lang]/[about]/index.tsx
+++ pages/[lang]/[about]/index.tsx
@@ -2,25 +2,33 @@ import { AppProps } from 'next/dist/next-server/lib/router/router';
 import locales from '../../../public/locales/locales';
 import * as matter from 'gray-matter';
 
-type Params = {
-  lang: String;
-  about: String;
-};
-
 type AboutProps = {
   post: any;
   body: string;
   title: string;
+  allData: AllData[];
 };
 
-const About = ({ post, body, title }: AboutProps) => {
+type AllData = {
+  slug: any;
+  title: {
+    [key: string]: any;
+  };
+  body: string;
+};
+
+const About = ({ post, body, title, allData }: AboutProps) => {
   return (
     <div>
       <h1>{post.about}</h1>
       <p>{post.about}</p>
 
-      <h2>{title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: body }} />
+      {allData.map((data) => (
+        <>
+          <h1>{data.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: data.body }} />
+        </>
+      ))}
     </div>
   );
 };
@@ -36,6 +44,30 @@ export const getStaticPaths = () => {
   };
 };
 
+export async function getAllTopics() {
+  const context = require.context(
+    '../../../public/locales/en-my/',
+    false,
+    /\.mdx$/
+  );
+  const posts = [];
+
+  console.log('1 - ', context.keys());
+
+  for (const key of context.keys()) {
+    const post = key.slice(2);
+    const { data, content } = matter.read(`public/locales/en-my/${post}`);
+
+    posts.push({
+      slug: post.replace('.mdx', ''),
+      title: data.title,
+      order: data.order,
+      body: content,
+    });
+  }
+  return posts;
+}
+
 export const getStaticProps = async ({ params }: AppProps) => {
   console.log('lang: ', params.lang);
   let lang: string = params.lang;
@@ -46,14 +78,17 @@ export const getStaticProps = async ({ params }: AppProps) => {
 
   let post = JSON.parse(JSON.stringify(locales[lang].default));
 
-  const { data, content } = matter.read('public/locales/en-my/topic.mdx');
+  const { data, content } = matter.read('public/locales/en-my/topic1.mdx');
   let title: string = data.title;
+
+  let allData = await getAllTopics();
 
   return {
     props: {
       post: post,
       body: content,
       title,
+      allData,
     },
   };
 };
